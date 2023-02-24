ab90ee7514a905f1a2d2c268772e4de21a3048b0
hadit10 <haditya.prasetyo.hp@gmail.com>
2023-02-24 10:53:47 +0700

    membuat file script.js dan update file index

---
+++ script.js
@@ -0,0 +1,14 @@
+
+alert('selamat datang di aplikasi perkalian sederhana');
+var lagi = true;
+while (lagi === true){
+    var angka1 = prompt('masukkan nominal 1:');
+    var angka2 = prompt('masukkan nominal 2:')
+    alert('jumlah perkalian adalah: ' + angka1*angka2);
+
+    lagi = confirm('coba lagi?');
+}
+alert('thanks');
+
+console.log ('Hello world');
+console.log ('isi variabel ini adalah' + " " + x);
